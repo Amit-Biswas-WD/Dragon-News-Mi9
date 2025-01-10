b4a2e8bca8abf69f1a8324894a7e9b041d083eee
Amit biswas <amitbiswas9282@gmail.com>
2025-01-10 23:30:08 +0600

    Find Us On

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -1,7 +1,20 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const FindUs = () => {
   return (
     <div>
       <h2 className="text-lg font-semibold my-4">Find Us On</h2>
+      <div className="join flex *:bg-white join-vertical">
+        <button className="btn justify-start join-item flex items-center gap-4">
+          <FaFacebook /> Facebook
+        </button>
+        <button className="btn justify-start join-item flex items-center gap-4">
+          <FaTwitter /> Twitter
+        </button>
+        <button className="btn justify-start join-item flex items-center gap-4">
+          <FaInstagram /> Instagram
+        </button>
+      </div>
     </div>
   );
 };
